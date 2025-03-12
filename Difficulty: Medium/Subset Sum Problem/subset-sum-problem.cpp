b6298d730ba624034a9e4f2b930e0fa30ecc2f79
Vibhuti Chandrakar <111688295+VC6378@users.noreply.github.com>
2025-03-12 14:59:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -5,33 +5,36 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function template for C++
 
 class Solution {
   public:
-  
-    bool f(vector<vector<int>>&dp,vector<int> &arr,int target,int ind){
-        if(target==0) return true;
-        if(ind==arr.size()){
-            if(target==0) return true;
-            return false;
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        bool take=false;
-        if(arr[ind]<=target){
-            take=f(dp,arr,target-arr[ind],ind+1);
-        }
-        bool nottake = f(dp,arr,target,ind+1);
-        return dp[ind][target]=(take || nottake);
-    }
-  
-    bool isSubsetSum(vector<int> arr, int sum) {
+    bool isSubsetSum(vector<int>& arr, int target) {
         // code here
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-        return f(dp,arr,sum,0);
+        vector<vector<int>> dp(n+1,vector<int>(target+1));
+        for(int i=0;i<=target;i++){
+            dp[0][i]=false;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(arr[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][target];
     }
 };
+
 
 //{ Driver Code Starts.
 
@@ -58,6 +61,7 @@ int main() {
             cout << "true" << endl;
         else
             cout << "false" << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
